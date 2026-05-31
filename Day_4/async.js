ac81b2130e4ec9a34afffb19e5de70c69c32f51c
pavan-ambareesh <pavanambareeshiv@zohomail.in>
2026-05-31 23:39:35 +0530

    Task 2 completed

--- Day_4/async.js
+++ Day_4/async.js
@@ -5,15 +5,37 @@
 console.log("1. Test started");
 setTimeout(() => {
     console.log("2. Login Page Loaded");
-} , 2000);
+} , );
 
 console.log("3. Moving to next step");
 
 // Task 2
 //Promise
 
-function fetchTestData() {
+function fetchTestData(testId) {
     return  new Promise((resolve,reject) => {
-        
-    })
+        setTimeout(() => {
+            if (testId === "TC001") {
+                resolve({id : "TC001" , staus : "passed"});
+            } else {
+                reject("Test case not found");
+            }
+        }, 2500);
+    });
 }
+
+// fetchTestData("TC001").then((result) =>{
+//     console.log("Success :", result);
+// })
+// .catch(error => {
+//     console.log("Error :", error);
+// });
+
+console.log("This Line runs before the promise is resoved or rejected");
+
+fetchTestData("TC999").then((result) =>{
+    console.log("Success :", result);
+})
+.catch(error => {
+    console.log("Error :", error);
+});
