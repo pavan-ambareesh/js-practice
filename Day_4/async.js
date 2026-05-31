7b4b8248ff0748ace6a1b64296196f1039bbbf52
pavan-ambareesh <pavanambareeshiv@zohomail.in>
2026-05-31 23:21:14 +0530

    Day_4 Task 1 push

---
+++ Day_4/async.js
@@ -0,0 +1,19 @@
+//Day 4
+// Task 1
+
+
+console.log("1. Test started");
+setTimeout(() => {
+    console.log("2. Login Page Loaded");
+} , 2000);
+
+console.log("3. Moving to next step");
+
+// Task 2
+//Promise
+
+function fetchTestData() {
+    return  new Promise((resolve,reject) => {
+        
+    })
+}
